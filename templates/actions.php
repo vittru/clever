711cf8170ca8cd10b11b579b017dce71c377057c
Vitaly Trusov <vitaly.trusov@gmail.com>
2016-11-03 11:56:12 +0400

    actions restored

--- templates/actions.php
+++ templates/actions.php
@@ -10,7 +10,23 @@ include 'header.php';
                 <h2>С днем рождения салона красоты "Каролина"</h2>
                 <div class="newstime">1/11/2016 - 14/11/2016</div>
                 <img src="/images/banners/Karolina.jpg">
+                <p></p>
+                <img src="images/actions/bonus.jpg">
                 <div>Полные условия акции читайте <a href="/actions/karolina2016">здесь</a></div>
+                <h2>Бесплатная доставка по Самаре заказов от 500 рублей</h2>
+                <div class="newstime">6/10/2016 - 1/11/2016</div>
+                <img src="/images/actions/action1.jpg">
+                <h2>Акции от компании МиКо в октябре</h2>
+                <div class="newstime">1/10/2016 - 1/11/2016</div>
+                <img src="/images/actions/Мед и малина_октябрь.jpg">
+                <p></p>
+                <img src="/images/actions/Роза_октябрь.jpg">
+                <h2>Акции от компании МиКо в сентябре</h2>
+                <div class="newstime">1/09/2016 - 1/10/2016</div>
+                <img src="/images/actions/Miko_action_bdg_medimalina.jpg">
+                <p></p>
+                <img src="/images/actions/Miko_action_lavanda-andiroba.jpg">
+                
             </div>
         </div>
     </div>
