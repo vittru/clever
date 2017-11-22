dc024753a8dde4177651f0d970fc28ce71914564
Vitaly Trusov <vitaly.trusov@gmail.com>
2017-11-22 11:58:22 +0400

    catalog/category with incorrect category name lead to 404

--- controllers/about.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-Class Controller_About Extends Controller_Base {
-
-    function index() {
-        $this->registry['model']->logVisit(9);
-        $this->registry['template']->show('about');
-    }
-}
--- templates/about.php
+++
@@ -1,6 +0,0 @@
-<?php
-include 'header.php';
-?>
-О нас
-<?php
-include 'footer.php';
