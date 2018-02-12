796129e726f09b0330c36b0ad30c425c339c5062
Vitaly Trusov <vitaly.trusov@gmail.com>
2018-02-12 23:00:26 +0400

    sorting adopted for mobiles

--- templates/sort.php
+++ templates/sort.php
@@ -2,8 +2,8 @@
     <div class="aa-product-catg-head-left">
         <div class="aa-sort-form">
             <label>Сортировка: </label>
-            <button id="nameSortButton" class="sort"> по названию <span class="sortDesc" hidden>&#x25B2;</span><span class="sortAsc" hidden>&#x25BC;</span></button>
-            <button id="priceSortButton" class="sort"> по цене <span class="sortDesc" hidden>&#x25B2;</span><span class="sortAsc" hidden>&#x25BC;</span></button>
+            <button id="nameSortButton" class="sort"> имя <span class="sortDesc" hidden>&#x25B2;</span><span class="sortAsc" hidden>&#x25BC;</span></button>
+            <button id="priceSortButton" class="sort"> цена <span class="sortDesc" hidden>&#x25B2;</span><span class="sortAsc" hidden>&#x25BC;</span></button>
         </div>
         <!--div class="aa-show-form">
             <label>Поиск: </label>
