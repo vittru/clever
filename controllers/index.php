f08abbded80db973387098262882ff2256756780
Vitaly Trusov <vitaly.trusov@gmail.com>
2018-02-20 17:23:29 +0400

    some changes for seo

--- controllers/index.php
+++ controllers/index.php
@@ -7,7 +7,7 @@ Class Controller_Index Extends Controller_Base {
         $this->registry['template']->set('firms', $this->registry['firms']);
         $pgoods = $this->registry['model']->getPopularGoods();
         $this->registry['template']->set('pgoods', $pgoods);
-        $this->registry['template']->set('metaTitle', 'Купить натуральную косметику в Самаре | Экомаркет Клевер');
+        $this->registry['template']->set('metaTitle', 'Купить натуральную косметику в Самаре по низкой цене | Экомаркет Клевер');
         $this->registry['template']->set('metaDescription', 'Купить натуральную органическую косметику в Самаре по низкой цене и с доставкой по городу в интернет-магазине Клевер.');
         $this->registry['template']->set('metaKeywords', 'интернет магазин, купить, натуральная косметика, органик, официальный сайт, органическая косметика, эко косметика, экомаркет, самара');
         $this->registry['template']->show('index');
