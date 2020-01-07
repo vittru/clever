1248264ae77e75aea782e405394938bc62f26dbc
Vitaly Trusov <vitaly.trusov@gmail.com>
2020-01-07 17:07:42 +0400

    fivGoodsDiscount activated

--- controllers/cart.php
+++ controllers/cart.php
@@ -68,7 +68,7 @@ Class Controller_Cart Extends Controller_Base {
     }  
     
     private function applyDiscounts() {
-        //$this->thirdFree();
+        $this->fiveGoodsDiscount();
     }
     
     //При покупке шампуня скидка на средства для волос 30%
