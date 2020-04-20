ff71694d4d299a168dc7eaf4a4777d6450b91f7a
Vitaly Trusov <vitaly.trusov@gmail.com>
2020-04-20 14:49:34 +0400

    action changed

--- controllers/cart.php
+++ controllers/cart.php
@@ -68,7 +68,7 @@ Class Controller_Cart Extends Controller_Base {
     }  
     
     private function applyDiscounts() {
-        $this->homeSecondHalf();
+        $this->thirdFree();
     }
     
     //При покупке шампуня скидка на средства для волос 30%
